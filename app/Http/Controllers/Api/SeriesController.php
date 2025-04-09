e0761b71ec4d7b12c8261cb2f2a9d4c054d0f9d4
Matheus Oliveira <contamatheusprofissional@gmail.com>
2025-04-09 19:19:38 -0300

    feat: adding create and show methods

--- app/Http/Controllers/Api/SeriesController.php
+++ app/Http/Controllers/Api/SeriesController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Api;
 
 use App\Models\Series;
+use App\Http\Requests\SeriesFormRequest;
+use App\Repositories\SeriesRepository;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
@@ -11,5 +13,15 @@ class SeriesController extends Controller
     public function index()
     {
         return Series::all();
+    }
+
+    public function store(SeriesFormRequest $request, SeriesRepository $repository)
+    {
+        return response()->json($repository->add($request->all()), 201);
+    }
+
+    public function show(int $id)
+    {
+        return Series::whereId($id)->with('seasons.episodes')->first();
     }
 }
