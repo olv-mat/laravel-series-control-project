d100f7507004e1cd6ee52f650b2bc7dc2dce06d3
Matheus Oliveira <contamatheusprofissional@gmail.com>
2025-05-04 09:32:43 -0300

    feat: creating authentication route

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Models\Series;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\{
+    LoginController,
     SeriesController,
     SeasonsController,
     EpisodesController,
@@ -13,7 +14,10 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::apiResource('/series', SeriesController::class);
-Route::get('/series/{series}/seasons', [SeasonsController::class, "show"]);
-Route::get('/series/{series}/episodes', [EpisodesController::class, "show"]);
-Route::patch('/episodes/{episode}', [EpisodesController::class, "update"]);
+Route::post('/login', [LoginController::class, "index"]);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('/series', SeriesController::class);
+    Route::get('/series/{series}/seasons', [SeasonsController::class, "show"]);
+    Route::get('/series/{series}/episodes', [EpisodesController::class, "show"]);
+    Route::patch('/episodes/{episode}', [EpisodesController::class, "update"]);
+});
